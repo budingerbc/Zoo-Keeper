4311a36dc139dc799ffb800fc3b91d92a72a584b
Ben Budinger <budingerbc@gmail.com>
2017-09-22 08:37:19 -0700

    Add animal class to contain data about tracked animals in zoo

---
+++ app/animal.ts
@@ -0,0 +1,18 @@
+let globalId: number = 0;
+
+export class Animal {
+
+  constructor(
+    public species: string,
+    public name: string,
+    public age: number,
+    public diet: string,
+    public location: string,
+    public caretakers: number,
+    public sex: string,
+    public like: string,
+    public dislike: string) {
+    }
+
+  public id: number = globalId++;
+}
